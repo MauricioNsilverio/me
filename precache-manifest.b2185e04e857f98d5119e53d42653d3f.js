12a521df5ca3f80ffe358da6ef3a6a07b741de42
mauricio@controlle.com <mauricionsilverio@gmail.com>
2019-08-29 21:41:54 -0300

    Updates

---
+++ precache-manifest.b2185e04e857f98d5119e53d42653d3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa1023034d3728129a658ad863f4133",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "0c1e6868bee487ac0cdf",
+    "url": "/me/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "cc95cf52ff9118fd5293",
+    "url": "/me/static/js/2.51678b07.chunk.js"
+  },
+  {
+    "revision": "0c1e6868bee487ac0cdf",
+    "url": "/me/static/js/main.4ff94ef0.chunk.js"
+  },
+  {
+    "revision": "515b16f1f0353ed3628d",
+    "url": "/me/static/js/runtime~main.5422a9c8.js"
+  }
+]);
